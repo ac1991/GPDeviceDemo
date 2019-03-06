0a4388068d738f4ace22913268599d49517c794e
WUSHENGQI\sqwu <441578157@qq.com>
2019-03-06 20:58:46 +0800

    新增权重配置

--- app/src/main/java/com/example/devicedemo/GP58MMIIITicketManager.java
+++ app/src/main/java/com/example/devicedemo/GP58MMIIITicketManager.java
@@ -44,7 +44,16 @@ public class GP58MMIIITicketManager {
                     esc.addUserCommand(TicketTextSize.FONT_SIZE_NORMAL.getGP58MMIIITextSize());
                     //默认居左打印
                     esc.addSelectJustification(TicketAlign.LEFT.getGp58MBIIIAlign());
-                    addTextRow(esc, columnLineBean.texts, null);
+
+                    if (columnLineBean.widths == null) {
+                        addTextRow(esc, columnLineBean.texts);
+                    }else {
+                        try {
+                            addTextRow(esc, columnLineBean.texts, columnLineBean.widths);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                     break;
             }
         }
@@ -82,42 +91,109 @@ public class GP58MMIIITicketManager {
     }
 
     /**
+     *添加一行数据
+     * @return
+     */
+    public static void addTextRow(EscCommand escCommand, String[] text){
+        try {
+            addTextRow(escCommand, text, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        int length = text.length;
+//
+//        int marginSize = 2;
+//
+//        //获取每列的宽度
+//        int columnWidth = maxSize / length - marginSize ;
+//
+//        Log.i("每列宽度：", columnWidth + "");
+//
+//        //获取文本的最大长度
+//        int maxTextLength = 0;
+//        for (int textIndex = 0; textIndex < length; textIndex++) {
+//            int tempLength = getStringLength(text[textIndex]);
+//            if (maxTextLength < tempLength) {
+//                maxTextLength = tempLength;
+//            }
+//            Log.i("获取最大长度", "maxTextLength:" + maxTextLength);
+//        }
+//
+//        Log.i("分行", "分行:" + (maxTextLength / columnWidth + 1));
+//        for (int i = 0; i < maxTextLength / columnWidth + 1; i++) {//分行
+//            Log.i("第几行：", i + "");
+//
+//            for (int index = 0; index < length; index++) {
+//                int textLength = getStringLength(text[index]);
+//                if (textLength > columnWidth * i) {
+//                    Log.i("第几列：", index + "");
+//
+//                    //设置绝对打印位置
+//                    escCommand.addSetAbsolutePrintPosition((short) (index * (columnWidth + marginSize)* 12));
+//                    escCommand.addText(getSubString(text[index], i * columnWidth, (i + 1) * columnWidth));
+//                }
+//            }
+//            escCommand.addPrintAndLineFeed();
+//        }
+    }
+
+    /**
      * 添加换行符
      * @return
      */
-    public static void addTextRow(EscCommand escCommand, String[] text, int[] weight){
-        int length = text.length;
+    public static void addTextRow(EscCommand escCommand, String[] texts, int[] weights) throws Exception {
+        int textArraySize = texts.length;
 
-        int marginSize = 2;
+        int marginSize = 2;//单位：字符
 
-        //获取每列的宽度
-        int columnWidth = maxSize / length - marginSize ;
-
-        Log.i("每列宽度：", columnWidth + "");
-
-        //获取文本的最大长度
-        int maxTextLength = 0;
-        for (int textIndex = 0; textIndex < length; textIndex++) {
-            int tempLength = getStringLength(text[textIndex]);
-            if (maxTextLength < tempLength) {
-                maxTextLength = tempLength;
+        if (weights == null){
+            weights = new int[textArraySize];
+            for (int i = 0; i < textArraySize; i++){
+                weights[i] = 1;
             }
-
-            Log.i("获取最大长度", "maxTextLength:" + maxTextLength);
         }
 
-        Log.i("分行", "分行:" + (maxTextLength / columnWidth + 1));
-        for (int i = 0; i < maxTextLength / columnWidth + 1; i++) {//分行
+        if (texts.length != weights.length){
+            throw new Exception("text.length != weight.length");
+        }
+
+        //总权重
+        int sumWeight = 0;
+        for (int i = 0; i< weights.length; i++){
+            sumWeight += weights[i];
+        }
+
+        int maxRowNum = 0;//获取最大行数
+        int[] textLengthColumns = new int[textArraySize]; //不同列文本的长度
+        int[] offsetColumns = new int[textArraySize]; //每列文本偏移量
+        int offsetSum = 0;
+        for (int textIndex = 0; textIndex < textArraySize; textIndex++) {
+            offsetColumns[textIndex] = offsetSum;
+            int textLengthColumn = (int) ((maxSize - marginSize * (textArraySize - 1)) * (((float)weights[textIndex] /(float)sumWeight))); //当前列文本的长度
+            offsetSum += textLengthColumn * 12 + marginSize * 12;
+
+            textLengthColumns[textIndex] = textLengthColumn;
+
+            int textLength = getStringLength(texts[textIndex]);//获取文本的长度
+            int tempRowNum = (textLength % textLengthColumn) > 0 ? (textLength/ textLengthColumn) + 1 : (textLength/ textLengthColumn);
+
+            if (maxRowNum < tempRowNum) {
+                maxRowNum = tempRowNum;
+            }
+            Log.i("获取最大行数", "maxRowNum:" + maxRowNum);
+        }
+
+        for (int i = 0; i < maxRowNum; i++) {//分行
             Log.i("第几行：", i + "");
 
-            for (int index = 0; index < length; index++) {
-                int textLength = getStringLength(text[index]);
-                if (textLength > columnWidth * i) {
+            for (int index = 0; index < textArraySize; index++) { //为每列文本设置被截取的字符串和位移量
+                int textLength = getStringLength(texts[index]);
+                if (textLength > i * textLengthColumns[index]) { //判断字符长度是否大于 行数 * 每行文本数，如果大于，则添加打印，如果小于，则表示已经无文本可打印
                     Log.i("第几列：", index + "");
 
                     //设置绝对打印位置
-                    escCommand.addSetAbsolutePrintPosition((short) (index * (columnWidth + marginSize)* 12));
-                    escCommand.addText(getSubString(text[index], i * columnWidth, (i + 1) * columnWidth));
+                    escCommand.addSetAbsolutePrintPosition((short) offsetColumns[index]);
+                    escCommand.addText(getSubString(texts[index], i * textLengthColumns[index], (i + 1)* textLengthColumns[index]));
                 }
             }
             escCommand.addPrintAndLineFeed();
